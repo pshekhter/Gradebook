0b392e446fd9cfeb74dda77704424d4a5b9972c3
pshekhter <pshekhter@gmail.com>
2017-03-01 12:12:26 -0500

    Modified code to fix problems.

---
+++ src/java/gradebook/InstructorController.java
@@ -0,0 +1,103 @@
+
+package gradebook;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+/**
+ *
+ * @author pshekhter
+ */
+@Named(value = "instructorController")
+@SessionScoped
+public class InstructorController implements Serializable {
+    
+    // Data values
+    DataModel instructorValues;
+    
+    // Instructor Helper
+    InstructorHelper instructorHelper;
+    
+    // Maps to components in template
+    Instructor instructor;
+    String instructorEmail;
+    int instructorID = 0;
+    int previousInstructorID;
+    String response;
+
+    /**
+     * Creates a new instance of InstructorController
+     */
+    public InstructorController() {
+        instructorHelper = new InstructorHelper();
+        previousInstructorID = instructorID;
+        response = "The selected instructor is: ";
+    }
+
+    public DataModel getInstructorValues() {
+        if (instructorValues == null) {
+            instructorValues = new ListDataModel(instructorHelper.getInstructors());
+        }
+        return instructorValues;
+    }
+
+    public void setInstructorValues(DataModel instructorValues) {
+        this.instructorValues = instructorValues;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    
+    public int getInstructorID() {
+        if (instructorID != previousInstructorID) previousInstructorID = instructorID;
+        return instructorID;
+    }
+
+    public void setInstructorID(int instructorID) {
+        this.previousInstructorID = this.instructorID;
+        this.instructorID = instructorID;
+    }
+
+    public String getResponse() {
+        if (instructorID == previousInstructorID) {
+            instructor = (Instructor)instructorHelper.getInstructors().get(instructorID);
+            instructorEmail = instructor.getInstructorEmail();
+            response = "The selected instructor is: ";
+            response = response.concat(instructorEmail);
+            return response;
+        } 
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public int getPreviousInstructorID() {
+        return previousInstructorID;
+    }
+
+    public void setPreviousInstructorID(int previousInstructorID) {
+        this.previousInstructorID = previousInstructorID;
+    }
+
+}
