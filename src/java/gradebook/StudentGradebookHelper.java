7a2284e8c88b28a9b3780494241c4857c010bfb8
pshekhter <pshekhter@gmail.com>
2017-03-11 18:59:00 -0500

    Modified StudentGradebookHelper

--- src/java/gradebook/StudentGradebookHelper.java
+++ src/java/gradebook/StudentGradebookHelper.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gradebook;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import java.util.List;
 
 /**
  *
- * @author iGenius
+ * @author Pavel Shekhter
  */
 public class StudentGradebookHelper {
+    
+    Session session = null;
+    
+    public StudentGradebookHelper() {
+        try {
+            // Initialize session and begin transaction with MySQL
+            this.session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
     
 }
